75ccf35aa535a699e89f4dc7c8c6c12db9da9048
Sam Johnson <s_johnso@yahoo.com>
2023-07-17 22:09:26 -0500

    added button functionality

---
+++ ToDo.md
@@ -0,0 +1,10 @@
+-[x] Add listener attack buttons
+    -[x] Modify HP and AP
+
+-[] Render new points to the DOM
+    -[] Update text above the AP and HP
+    -[] Set HP and AP so they cannot reach 0
+    -[] if HP = 0 replace class to 'dead'
+    -[] if you run out of AP replace 'walk' class with 'jump'
+    -[] if AP reaches 0 disable all attack buttons
+
--- script.js
+++ script.js
@@ -5,14 +5,65 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let yourAP = 100
+let mushHP = 100
+
+
 function onReady() {
     
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
+    // Attack butotn listener/handlers
+    $('.attack-btn').on('click', handleAttack);
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+    // call render
+
+}
+// Arcane Button click function
+function handleAttack() {
+    if ($(this).hasClass('arcane-scepter')) {
+        yourAP -= 12;
+        mushHP -= 14;
+    } else if ($(this).hasClass('entangle')) {
+        yourAP -= 23;
+        mushHP -= 9;
+    } else if ($(this).hasClass('dragon-blade')) {
+        yourAP -= 38;
+        mushHP -= 47;
+    } else if ($(this).hasClass('star-fire')) {
+        yourAP -= 33;
+        mushHP -= 25;
+    } 
+
+    // render new AP and HP
+    render ();
+}
+
+function render() {
+    // check to see if AP or HP are below 0
+    if (mushHP <= 0) {
+        $('.ap-text').text(`
+        0 AP
+    `);
+        $('.freaky-fungus').addClass('dead').removeClass('walk');
+        $('.attack-btn').attr('disabled', true);
+    } else if (mushHP > 0) {
+        $('.hp-text').text(`
+        ${mushHP} HP
+    `)
+    } if (yourAP <= 0) {
+        $('.ap-text').text(`
+        0 AP
+    `);
+        $('.freaky-fungus').addClass('jump').removeClass('walk');
+        $('.attack-btn').attr('disabled', true);
+    } else if (yourAP > 0) {
+        $('.ap-text').text(`
+        ${yourAP} AP
+    `)
+    }
+
 }
